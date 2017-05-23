d1d6622681e14eddc8c25c0d3807a51849e8bc00
Del Dewar <derek.dewar@wipro.com>
2017-05-23 10:29:21 +0100

    re-introduce other test

--- test/specs/login.js
+++ test/specs/login.js
@@ -2,19 +2,19 @@ var expect = require('chai').expect;
 var LoginPage = require('../pageobjects/login.page');
 
 describe('The login page', function() {
-    // it('should allow access with valid credentials', function() {
-    //     LoginPage.open();
-    //     LoginPage.username.setValue('invalid_user');
-    //     LoginPage.password.setValue('invalid_password');
-    //     LoginPage.submit();
-    //
-    //     expect(LoginPage.flash.getText()).to.contain('Your username is invalid!');
-    // });
+    it('should allow access with valid credentials', function() {
+        LoginPage.open();
+        LoginPage.username.setValue('invalid_user');
+        LoginPage.password.setValue('invalid_password');
+        LoginPage.submit();
+
+        expect(LoginPage.flash.getText()).to.contain('Your username is invalid!');
+    });
 
     it('should refuse access with invalid credentials', function() {
         LoginPage.open();
         LoginPage.username.setValue('tomsmith');
-        LoginPage.password.setValue('SuperSecretPassword');
+        LoginPage.password.setValue('SuperSecretPassword'); // TODO - Deliberate failure to play with screenshots
         LoginPage.submit();
 
         LoginPage.flash.waitForExist();
