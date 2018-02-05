b6af381836fd3ea61654aa4f3e796e59a668d2c3
Del Dewar <derek.dewar@wipro.com>
2018-02-05 15:22:19 +0000

    Replaced wildcard in wdio conf

--- conf/wdio.conf.js
+++ conf/wdio.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/br*.js'
+        './test/specs/*.js'
     ],
     // Patterns to exclude.
     exclude: [
