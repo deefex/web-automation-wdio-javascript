264b74753c44e625962803f1b3dd32c00557db9f
Del Dewar <derek.dewar@wipro.com>
2018-02-05 15:03:51 +0000

    Fixed the broken images test

--- test/specs/broken_images_spec.js
+++ test/specs/broken_images_spec.js
@@ -3,18 +3,23 @@ var BrokenImagesPage = require('../pageobjects/broken_images_page');
 
 describe('Broken Images: https://the-internet.herokuapp.com/broken_images', function() {
 
-    it('not working yet', function() {
+    it('check that the page has one or more broken images', function() {
         BrokenImagesPage.open();
 
         // Try and execute this in the browser to see if we can get back images to iterate over.
         var brokenImages = browser.execute(function () {
-            document.getElementsByTagName('img');
+            var imgs =  document.getElementsByTagName('img');
+	        for (var i = 0; i < imgs.length; i++) {
+	            // Iterate over the images - if any of them have natural widths or heights == 0 return true
+		        if (imgs[i].naturalHeight == 0 || imgs[i].naturalWidth == 0) {
+			        return true;
+		        }
+	        }
+	        // All the images have non-zero naturalWidths/naturalHeights?
+	        return false;
         });
 
-        // print the result
-        console.log(brokenImages);
-
-
+        expect(brokenImages.value).to.equal(true);
     });
 
 });
