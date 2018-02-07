cc0f569fe824f9533d6dd398cc1fd29aac3a8898
Del Dewar <derek.dewar@wipro.com>
2018-02-07 14:04:13 +0000

    Added drag and drop tests

---
+++ test/pageobjects/drag_and_drop_page.js
@@ -0,0 +1,29 @@
+var dragAndDrop = require('html-dnd').codeForSelectors;
+var Page = require('./page');
+
+var dragAndDropPage = Object.create(Page, {
+
+	// Page object elements
+	column1:     { get: function () { return browser.element('#column-a'); } },
+	column2:     { get: function () { return browser.element('#column-b'); } },
+	column1Text: { get: function () { return browser.getText('#column-a > header'); } },
+	column2Text: { get: function () { return browser.getText('#column-b > header'); } },
+
+	// Define or overwrite page methods
+	open:   { value: function () {
+			Page.open.call(this, 'drag_and_drop');
+		}
+	},
+
+	dragAtoB: { value: function() {
+		browser.execute(dragAndDrop, '#column-a', '#column-b');
+		}
+	},
+
+	dragBtoA: { value: function() {
+			browser.execute(dragAndDrop, '#column-b', '#column-a');
+		}
+	}
+});
+
+module.exports = dragAndDropPage;
---
+++ test/specs/drag_and_drop_spec.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect;
+var DragAndDropPage = require('../pageobjects/drag_and_drop_page');
+
+describe('Drag & Drop: https://the-internet.herokuapp.com/drag_and_drop', function() {
+
+	it('I can successfully drag the element from column1 to column2', function() {
+		DragAndDropPage.open();
+		DragAndDropPage.dragAtoB();
+		expect(DragAndDropPage.column1Text).to.equal('B');
+	});
+
+	it('I can successfully drag the element from column2 to column1', function() {
+		DragAndDropPage.open();
+		DragAndDropPage.dragBtoA();
+		expect(DragAndDropPage.column2Text).to.equal('A');
+	});
+});
