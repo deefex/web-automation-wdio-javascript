59cf518829fb1e4022bde08fa9e26872f4e7d047
Del Dewar <deefex@kinloch.lan>
2018-02-07 22:42:39 +0000

    Added dynamic content

---
+++ test/pageobjects/dynamic_content_page.js
@@ -0,0 +1,18 @@
+var Page = require('./page');
+
+var dynamicContentPage = Object.create(Page, {
+
+    // Page object elements
+    row1Img:  { get: function () { return browser.getAttribute('//*[@id="content"]/div[1]/div[1]/img', 'src'); } },
+    row1Txt:  { get: function () { return browser.getText('//*[@id="content"]/div[1]/div[2]'); } },
+    row2Img:  { get: function () { return browser.getAttribute('//*[@id="content"]/div[2]/div[1]/img', 'src'); } },
+    row2Txt:  { get: function () { return browser.getText('//*[@id="content"]/div[2]/div[2]'); } },
+
+    // Define or overwrite page methods
+    open:   { value: function () {
+            Page.open.call(this, 'dynamic_content?with_content=static');
+        }
+    }
+});
+
+module.exports = dynamicContentPage;
