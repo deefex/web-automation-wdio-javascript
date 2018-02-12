67038794decbd5ccd6e8f2a492ef40cf4e308ff3
Del Dewar <deefex@kinloch.lan>
2018-02-12 20:51:34 +0000

    Added the exit intent test

---
+++ test/pageobjects/exit_intent_page.js
@@ -0,0 +1,23 @@
+var Page = require('./page');
+
+var exitIntentPage = Object.create(Page, {
+
+    // Page object elements
+    modal:       { get: function () { return browser.element('#ouibounce-modal'); } },
+
+    // Define or overwrite page methods
+    open:   { value: function () {
+            Page.open.call(this, 'exit_intent');
+        }
+    },
+
+    showModal: { value: function () {
+        browser.execute( function() {
+                $('#ouibounce-modal').show();
+            })
+        }
+    }
+
+});
+
+module.exports = exitIntentPage;
---
+++ test/specs/exit_intent_spec.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect;
+var ExitIntentPage = require('../pageobjects/exit_intent_page');
+
+describe('Exit Intent: https://the-internet.herokuapp.com/exit_intent', function() {
+
+    // The trigger that causes the modal to appear is to move the mouse outside the
+    // browser viewport, however it's not possible to perform that action with selenium
+    // as all actions are inherently *in* the viewport. Therefore, this test is a bit
+    // farcical, since all we're doing is revealing the modal and checking that it's visible
+
+    it('check that the modal is visible', function() {
+        ExitIntentPage.open();
+        ExitIntentPage.showModal();
+        expect(ExitIntentPage.modal.isVisible()).to.equal(true);
+    });
+
+});
