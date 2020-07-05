39657840724c90dc222b866de39afb7998cde13a
Del Dewar <del.dewar@gmail.com>
2020-07-05 11:38:10 +0100

    Fixed file upload spec

--- test/pageobjects/file_upload_page.js
+++ test/pageobjects/file_upload_page.js
@@ -1,31 +1,42 @@
 let Page = require('./page');
+let path = require('path');
 
 let fileUploaderPage = Object.create(Page, {
 
-  // Page object elements
-  pageHeading:      { get: function () { return $('h3'); } },
-  chooseSelector:   { get: function () { return '#file-upload'; } },
-  chooseButton:     { get: function () { return $('#file-upload'); } },
-  chooseValue:      { get: function () { return $('#file-upload').getValue(); } },
-  uploadButton:     { get: function () { return $('#file-submit'); } },
-  uploadedFiles:    { get: function () { return $('#uploaded-files'); } },
-  uploadedFilesText:{ get: function () { return $('#uploaded-files').getText(); } },
+    // Page object elements
+    pageHeading:      { get: function () { return $('h3'); } },
+    chooseSelector:   { get: function () { return '#file-upload'; } },
+    chooseButton:     { get: function () { return $('#file-upload'); } },
+    chooseValue:      { get: function () { return $('#file-upload').getValue(); } },
+    uploadButton:     { get: function () { return $('#file-submit'); } },
+    uploadedFiles:    { get: function () { return $('#uploaded-files'); } },
+    uploadedFilesText:{ get: function () { return $('#uploaded-files').getText(); } },
 
-  // Define or overwrite page methods
-  open:   { value: function () {
-      Page.open.call(this, 'upload');
-      this.waitForPageToLoad();
-    } },
+    // Define or overwrite page methods
+    open:   { value: function () {
+            Page.open.call(this, 'upload');
+            this.waitForPageToLoad();
+        } },
 
-  waitForPageToLoad: { value: function() {
-      this.pageHeading.waitForDisplayed(undefined, false, "fileUploaderPage: Mandatory element not found.");
+    waitForPageToLoad: { value: function() {
+            this.pageHeading.waitForDisplayed(undefined, false, "fileUploaderPage: Mandatory element not found.");
+        }
+    },
+
+    waitForMessage: { value: function() {
+            this.uploadedFiles.waitForExist();
+        }
+    },
+
+    uploadFile: { value: function() {
+            // eslint-disable-next-line no-undef
+            let toUpload = path.join(__dirname, '..', 'resources', 'spider-man.png');
+            let filePath = browser.uploadFile(toUpload);
+            this.chooseButton.setValue(filePath);
+            this.uploadButton.click();
+            this.waitForMessage();
+        }
     }
-  },
-
-  waitForMessage: { value: function() {
-      this.uploadedFiles.waitForExist();
-    } }
-
 });
 
 module.exports = fileUploaderPage;
